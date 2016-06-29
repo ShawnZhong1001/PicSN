088e4688d23a8536a9da1eb056f40f2427cdfca1
Shawn Zhong <1548445477@qq.com>
2016-06-29 19:23:00 +0800

    add manager.py using flask-script

---
+++ manager.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from flask_script import Manager
+from ex2 import app
+
+manager = Manager(app)
+
+
+# 为了交互,传入更多的参数
+@manager.option('-n', '--name', dest='name', default='nowcoder')
+def hello(name):
+    print 'hello', name
+
+
+@manager.command
+def init_database():
+    'initilize database'
+    print 'database...'
+
+
+if __name__ == '__main__':
+    manager.run()
